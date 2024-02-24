058d9533d6602cb0f323cc3e098c61e24ca02cd5
martin <martin2288@protonmail.com>
2024-02-24 17:13:43 -0300

    trabajo final

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
 import Banner from "./components/Banner"
+import Cart from "./components/Cart"
+import Error404 from "./components/Error404"
 import Footer from "./components/Footer"
 import ItemDetailContainer from "./components/ItemDetailContainer"
 import ItemListContainer from "./components/ItemListContainer"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar"
-import Error404 from "./components/Error404";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CartContextProvider from "./components/context/CartContext"
+import HelmetTitle from "./components/HelmetTitle"
+import Checkout from "./components/Checkout"
 
 const App = () => {
   return (
-    <BrowserRouter>
-  <NavBar />
-      <Routes>
-<Route path="/" element={<ItemListContainer />} />
-<Route path="/category/:id" element={<ItemListContainer />} />
-<Route path="/item/:id" element={<ItemDetailContainer />} />
-<Route path="*" element={<Error404 />} />
-      </Routes>
-      <Banner />
-      <Footer />
-    </BrowserRouter>
+    <CartContextProvider>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path={"/"} element={<><HelmetTitle title="LaFive - Helados, Chocolates, Panes, Quesos, BoneBroth, Hamburguesas. Todo Orgánico" /><ItemListContainer /></>} />
+          <Route path={"/category/:id"} element={<><HelmetTitle title="La Five - Productos" /><ItemListContainer /></>} />
+          <Route path={"/item/:id"} element={<ItemDetailContainer />} />
+          <Route path={"/cart"} element={<Cart />} />
+          <Route path={"/checkout"} element={<Checkout />} />
+          <Route path={"*"} element={<Error404 />} />
+        </Routes>
+        <Banner />
+        <Footer />
+      </BrowserRouter>
+    </CartContextProvider>
   )
 }
 
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,19 +1,23 @@
 import { useState } from "react";
 import ItemCount from "./ItemCount";
-import arrayProductos from "./json/productos.json";
+//import arrayProductos from "./json/productos.json";
 import { useEffect } from "react";
 import ItemList from "./ItemList";
+import { getFirestore, collection, query, where, getDocs } from "firebase/firestore";
 import { useParams } from "react-router-dom";
 import Carousel from "./Carousel";
+import Loading from "./Loading";
+//import { getFirestore, collection, addDoc } from "firebase/firestore";
 
 
 const ItemListContainer = () => {
     const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(true);
     const {id} =useParams();
 
 
-
-    useEffect(() => {
+                        //antes cuando usaba json
+/*     useEffect(() => {
         const promesa = new Promise(resolve => {
             setTimeout(() => {
                 resolve(id? arrayProductos.filter(item => item.categoria ===id) :arrayProductos);
@@ -23,16 +27,46 @@ const ItemListContainer = () => {
             setItems(data);
             console.log(data);
         })
+    }, [id]); */
+
+                                                            //subida de archivos en firebase itemslistcontainer
+/*                              useEffect(() => {
+                                const db = getFirestore();
+                                const itemsCollection = collection(db, "aca la coleccion");
+
+                                arrayProductos.forEach(producto => {
+                                    addDoc(itemsCollection, producto);
+                                });
+
+                                console.log("Los productos se subieron correctamente!");
+                            },[])  
+ */
+
+
+
+
+
+    // Llamada de Productos desde el Firestore
+    useEffect(() => {
+        const db = getFirestore();
+        const itemsCollection = collection(db, "items");
+        const consulta = id ? query(itemsCollection, where("categoria", "==", id)) : itemsCollection;
+        getDocs(consulta).then(resultado => {
+            setLoading(false);
+            setItems(resultado.docs.map(producto => ({id:producto.id, ...producto.data()})));
+        });
     }, [id]);
+
+
+
 
     return (
         <>
- 
-            <ItemList items={items} />
+
+        
             {id ? "" : <Carousel />}
-   
+            {loading ? <Loading /> : <ItemList items={items} />}
         </>
     )
 }
-
 export default ItemListContainer;
